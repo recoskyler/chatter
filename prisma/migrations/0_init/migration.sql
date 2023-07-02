527d129ce747f422a9eb98c2667a2adfc6147fa4
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-07-02 14:46:19 +0000

    Generated migration with auth

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -27,6 +27,50 @@ CREATE TABLE `Prompt` (
     PRIMARY KEY (`uuid`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `auth_user` (
+    `id` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(64) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+
+    UNIQUE INDEX `auth_user_id_key`(`id`),
+    UNIQUE INDEX `auth_user_username_key`(`username`),
+    UNIQUE INDEX `auth_user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `auth_session` (
+    `id` VARCHAR(191) NOT NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `active_expires` BIGINT NOT NULL,
+    `idle_expires` BIGINT NOT NULL,
+
+    UNIQUE INDEX `auth_session_id_key`(`id`),
+    INDEX `auth_session_user_id_idx`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `auth_key` (
+    `id` VARCHAR(191) NOT NULL,
+    `hashed_password` VARCHAR(191) NULL,
+    `user_id` VARCHAR(191) NOT NULL,
+    `primary_key` BOOLEAN NOT NULL,
+    `expires` BIGINT NULL,
+
+    UNIQUE INDEX `auth_key_id_key`(`id`),
+    INDEX `auth_key_user_id_idx`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `Prompt` ADD CONSTRAINT `Prompt_ibfk_1` FOREIGN KEY (`chatUUID`) REFERENCES `Chat`(`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `auth_session` ADD CONSTRAINT `auth_session_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `auth_user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `auth_key` ADD CONSTRAINT `auth_key_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `auth_user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
