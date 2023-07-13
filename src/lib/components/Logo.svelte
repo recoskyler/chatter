040441d99d1078200f4ba19a0c10de80f3d90352
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-07-13 00:52:05 +0000

    Linting and formatting

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,25 +1,23 @@
 <div class="logo">
-    <a href="https://kit.svelte.dev">
-        Chatter
-    </a>
+  <a href="https://kit.svelte.dev"> Chatter </a>
 </div>
 
 <style lang="scss">
   .logo {
     margin-bottom: 2rem;
 
-    >a {
-        color: var(--color-theme-2) !important;
-        font-size: 2rem;
-        font-weight: 600;
-        line-height: 2.5rem;
-        transition: all 200ms ease-in-out;
+    > a {
+      color: var(--color-theme-2) !important;
+      font-size: 2rem;
+      font-weight: 600;
+      line-height: 2.5rem;
+      transition: all 200ms ease-in-out;
     }
 
-    >a:hover {
-        letter-spacing: 0.3ch;
-        font-weight: 600;
-        text-decoration: none;
+    > a:hover {
+      letter-spacing: 0.3ch;
+      font-weight: 600;
+      text-decoration: none;
     }
   }
 </style>
--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,1 +1,2 @@
+// eslint-disable-next-line no-unused-vars
 declare let __APP_VERSION__: string;
