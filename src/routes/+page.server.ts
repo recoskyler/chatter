e917153e3bf989cafaa20c049cb5e42920cfe0f4
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-07-13 12:41:08 +0000

    Formatting

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,8 +6,11 @@ import { auth } from "$lib/server/lucia";
 export const actions: Actions = {
   default: async ({ locals }) => {
     const { session } = await locals.auth.validateUser();
+
     if (!session) return fail(401);
+
     await auth.invalidateSession(session.sessionId); // invalidate session
+
     locals.auth.setSession(null); // remove cookie
   },
 };
