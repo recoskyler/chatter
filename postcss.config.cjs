85271c6dfba44be5e67a46f4a07e84e82f8dabdb
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-07-13 22:30:51 +0000

    Added Skeleton UI

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,14 @@
-module.exports = {
-    plugins: {
-        autoprefixer: {},
-    },
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+
+const config = {
+  plugins: [
+    //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+    // @ts-ignore
+    tailwindcss(),
+    //But others, like autoprefixer, need to run after,
+    autoprefixer,
+  ],
 };
+
+module.exports = config;
