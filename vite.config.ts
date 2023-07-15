b3b1d813f2424d06a688dcbbffd5f827c8fbb857
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-07-15 21:11:32 +0000

    Linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    define: {
-        '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
-    }
+  plugins: [
+    sveltekit(),
+  ],
+  define: { "__APP_VERSION__": JSON.stringify(process.env.npm_package_version) },
 });
