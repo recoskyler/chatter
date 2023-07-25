80ef495512774ed943f06f325d62f41438557b98
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-07-25 23:58:08 +0000

    Added error array display support

--- src/lib/components/FormError.svelte
+++ src/lib/components/FormError.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
   import { fade } from "svelte/transition";
 
-  export let error: string | undefined;
+  export let error: string | string[] | undefined;
 </script>
 
 {#if error && error.length > 0}
-  <p class="text-center text-red-400 my-5 break-words max-w-xs" transition:fade>{error}</p>
+  {#if typeof error === "string"}
+    <p class="text-center text-red-400 my-3 break-words max-w-xs" transition:fade>{error}</p>
+  {:else}
+    <div class="flex flex-col gap-3 items-center">
+      {#each error as err}
+        <p class="text-center text-red-400 my-3 break-words max-w-xs" transition:fade>{err}</p>
+      {/each}
+    </div>
+  {/if}
 {/if}
