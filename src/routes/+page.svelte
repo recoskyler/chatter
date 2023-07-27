c4d74f0d8a04546cf07c807b4a85faf5b048e33e
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-07-27 21:07:27 +0000

    Added temporary UI

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { version } from '$app/environment';
+</script>
+
+<div class="flex my-auto mx-auto items-center justify-center text-center gap-5">
+  <a href="/app/profile" class="anchor">Profile</a>
+  <a href="/app" class="anchor">App</a>
+  <p>v{version}</p>
+</div>
+
---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { pageTitle } from "writables/pageTitle";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+
+  $pageTitle = "Chatter";
+</script>
+
+<svelte:head>
+  <meta name="robots" content="noindex" />
+  <title>Chatter | Setup</title>
+</svelte:head>
+
+{#if data.user.chats.length === 0}
+  <div class="h-full w-full flex flex-col items-center justify-center">
+    <p class="text-center text-slate-400">You don't have any chats</p>
+
+    <form action="" method="post"></form>
+  </div>
+{/if}
