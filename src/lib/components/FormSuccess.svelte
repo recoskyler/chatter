5b60b3ef2992fcc93729fa2b42578917f8e04c23
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-07-28 23:35:57 +0000

    Created form success message component

---
+++ src/lib/components/FormSuccess.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+
+  export let message: string | string[] | undefined;
+</script>
+
+{#if message && message.length > 0}
+  {#if typeof message === "string"}
+    <p class="text-center text-green-400 my-3 break-words w-full max-w-xs" transition:fade>{message}</p>
+  {:else}
+    <div class="flex flex-col gap-3 items-center">
+      {#each message as msg}
+        <p class="text-center text-green-400 my-3 break-words w-full max-w-xs" transition:fade>{msg}</p>
+      {/each}
+    </div>
+  {/if}
+{/if}
