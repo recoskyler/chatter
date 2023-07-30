663d87c43957e38ec16dfa25cb06dea5aefb4810
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-07-30 03:21:25 +0000

    Added accounts page

--- src/routes/app/accounts/+page.server.ts
+++ src/routes/app/accounts/+page.server.ts
@@ -1,9 +1,16 @@
-import { error, redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import {
+  error, fail, redirect,
+} from "@sveltejs/kit";
+import type { Actions, PageServerLoad } from "./$types";
 import { db } from "$lib/server/drizzle";
-import { user } from "$lib/db/schema";
+import { user, userConfig } from "$lib/db/schema";
 import { eq } from "drizzle-orm";
 import { EMAIL_VERIFICATION } from "$lib/constants";
+import { z } from "zod";
+import { setError, superValidate } from "sveltekit-superforms/server";
+import { accountUpdateLimiter } from "$lib/server/limiter";
+
+const schema = z.object({ accountId: z.string().uuid().nullable() });
 
 export const load: PageServerLoad = async ({ locals }) => {
   const session = await locals.auth.validate();
@@ -16,14 +23,59 @@ export const load: PageServerLoad = async ({ locals }) => {
     with: {
       accounts: { with: { chatModel: true } },
       chats: true,
-      config: { with: { defaultAccount: true } },
+      config: true,
     },
     where: eq(user.id, session.user.userId),
   });
 
   if (!dbUser) throw error(404, "User not found");
 
-  if (dbUser.config === null) throw redirect(302, "/app/setup");
+  if (!dbUser.config) throw redirect(302, "/app/setup");
 
-  return { user: dbUser };
+  const form = await superValidate(schema);
+
+  form.data.accountId = dbUser.config.defaultAccountId;
+
+  return { user: dbUser, form };
+};
+
+export const actions: Actions = {
+  default: async event => {
+    if (await accountUpdateLimiter.isLimited(event)) throw error(429, "Too many requests");
+
+    const { locals, request } = event;
+    const session = await locals.auth.validate();
+
+    if (!session || (EMAIL_VERIFICATION && !session.user.verified)) {
+      throw redirect(302, "/login");
+    }
+
+    const form = await superValidate(request, schema);
+
+    if (!form.valid) {
+      console.error("Form invalid");
+      console.error(form.errors);
+      return fail(400, { form });
+    }
+
+    try {
+      const dbUser = await db.query.user.findFirst({
+        with: { config: true },
+        where: eq(user.id, session.user.userId),
+      });
+
+      if (!dbUser) throw error(404, "User not found");
+
+      await db.update(userConfig)
+        .set({ defaultAccountId: form.data.accountId })
+        .where(eq(userConfig.userId, session.user.userId));
+    } catch (e) {
+      console.error("Failed to create account");
+      console.error(e);
+
+      return setError(form, "", "Failed to create account");
+    }
+
+    return { form };
+  },
 };
--- src/routes/app/accounts/+page.svelte
+++ src/routes/app/accounts/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
-  import { pageTitle } from "writables/pageTitle";
   import type { PageData } from "./$types";
+  import Fa from "svelte-fa";
+  import { faPencil, faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+  import { superForm } from "sveltekit-superforms/client";
+  import { writable } from "svelte/store";
+  import FormError from "components/FormError.svelte";
 
   export let data: PageData;
 
-  $pageTitle = "Accounts";
+  const changed = writable(false);
+
+  const { form, enhance, delayed, errors } = superForm(data.form, {
+    onUpdated: () => {
+      $changed = false;
+    },
+  });
+
+  const availableAccounts = data.user.accounts.filter((a) => !a.deleted);
 </script>
 
 <svelte:head>
@@ -12,4 +24,97 @@
   <title>Chatter | Accounts</title>
 </svelte:head>
 
+<div class="h-full flex items-center p-5 flex-col gap-5">
+  <form use:enhance method="post" class="w-full max-w-lg mx-auto p-5">
+    <label class="label">
+      <span>Default account</span>
 
+      <select
+        class="select w-full"
+        name="accountId"
+        disabled={$delayed}
+        on:change={(_) => {
+          $changed = true;
+        }}
+        bind:value={$form.accountId}
+      >
+        {#each availableAccounts as account}
+          <option value={account.id}>{account.name}</option>
+        {/each}
+      </select>
+
+      {#if $errors.accountId}
+        <FormError error={$errors.accountId} />
+      {/if}
+
+      {#if $errors._errors}
+        <FormError error={$errors._errors} />
+      {/if}
+    </label>
+
+    <input
+      type="submit"
+      value={$delayed ? "Saving..." : "Save"}
+      class={`btn mt-5 w-full ${
+        $delayed || !$changed ? "variant-filled-surface" : "variant-filled"
+      }`}
+      disabled={$delayed || !$changed}
+    />
+  </form>
+
+  <div class="container mx-auto max-w-lg p-3">
+    {#if data.user.accounts}
+      <dl class="list-dl" />
+
+      <nav class="list-nav">
+        <ul>
+          <li>
+            <a href={`/app/accounts/create`}>
+              <span class="badge text-secondary-400">
+                <Fa icon={faPlus} />
+              </span>
+
+              <span class="flex-auto">
+                <span><strong>Create new account</strong></span>
+              </span>
+            </a>
+          </li>
+
+          {#each data.user.accounts as account}
+            <li>
+              <a href={`/app/accounts/${account.id}`}>
+                <span class="badge text-secondary-400">
+                  {#if account.deleted}
+                    <Fa icon={faTrash} />
+                  {:else}
+                    <Fa icon={faPencil} />
+                  {/if}
+                </span>
+
+                <span class="flex-auto">
+                  <span class={account.deleted ? "text-slate-400" : ""}>
+                    {account.name}
+                  </span>
+                </span>
+
+                <span
+                  class={`badge ml-3 ${
+                    account.deleted
+                      ? "variant-filled-error"
+                      : data.user.config.defaultAccountId === account.id
+                      ? "variant-filled-primary"
+                      : "variant-filled"
+                  }`}
+                >
+                  {account.chatModel.displayName}
+                </span>
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </nav>
+    {:else}
+      <p class="text-slate-400">You don't have any accounts</p>
+    {/if}
+  </div>
+</div>
