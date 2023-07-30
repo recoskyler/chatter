02764804a7cd242c803b9209b55817835970c2bd
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-07-30 03:21:12 +0000

    Added view account page

---
+++ src/routes/app/accounts/[id]/+page.server.ts
@@ -0,0 +1,194 @@
+import {
+  error, fail, redirect,
+} from "@sveltejs/kit";
+import { db } from "$lib/server/drizzle";
+import {
+  account, chatModel, user, userConfig,
+} from "$lib/db/schema";
+import { and, eq } from "drizzle-orm";
+import { EMAIL_VERIFICATION } from "$lib/constants";
+import { accountUpdateLimiter } from "$lib/server/limiter";
+import type { Actions, PageServerLoad } from "./$types";
+import {
+  message, setError, superValidate,
+} from "sveltekit-superforms/server";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().min(1).max(255),
+  key: z.string().min(1).max(255),
+  chatModelId: z.string().uuid(),
+});
+
+export const load: PageServerLoad = async event => {
+  accountUpdateLimiter.cookieLimiter?.preflight(event);
+
+  const { locals, params } = event;
+  const session = await locals.auth.validate();
+
+  if (!session || (EMAIL_VERIFICATION && !session.user.verified)) {
+    throw redirect(302, "/login");
+  }
+
+  const dbUser = await db.query.user.findFirst({
+    with: {
+      accounts: {
+        with: { chatModel: true },
+        where: eq(account.id, params.id),
+      },
+      config: { with: { defaultAccount: true } },
+    },
+    where: eq(user.id, session.user.userId),
+  });
+
+  const dbChatModels = await db.query.chatModel.findMany({ where: eq(chatModel.enabled, true) });
+
+  if (!dbUser) throw error(404, "User not found");
+
+  if (dbUser.accounts.length === 0) throw error(404, "Account not found");
+
+  const [dbAccount] = dbUser.accounts;
+
+  const form = await superValidate(dbAccount, schema);
+
+  return { user: dbUser, account: dbAccount, chatModels: dbChatModels, form };
+};
+
+export const actions: Actions = {
+  save: async event => {
+    if (await accountUpdateLimiter.isLimited(event)) throw error(429, "Too many requests");
+
+    const { locals, params, request } = event;
+    const session = await locals.auth.validate();
+
+    if (!session || (EMAIL_VERIFICATION && !session.user.verified)) {
+      throw redirect(302, "/login");
+    }
+
+    const form = await superValidate(request, schema);
+
+    if (!form.valid) {
+      console.error("Form invalid");
+      console.error(form.errors);
+      return fail(400, { form });
+    }
+
+    try {
+      const dbAccount = await db.query.account.findFirst(
+        { where: and(eq(account.id, params.id), eq(account.deleted, false)) },
+      );
+
+      if (!dbAccount) {
+        return setError(
+          form,
+          "",
+          "Cannot change deleted account. Restore first to make any changes.",
+        );
+      }
+
+      await db.update(account).set(form.data).where(eq(account.id, params.id));
+    } catch (e) {
+      return setError(form, "", "Failed to save changes");
+    }
+
+    return message(form, "Successfully saved changes");
+  },
+  delete: async event => {
+    if (await accountUpdateLimiter.isLimited(event)) throw error(429, "Too many requests");
+
+    const { locals, params } = event;
+    const session = await locals.auth.validate();
+
+    if (!session || (EMAIL_VERIFICATION && !session.user.verified)) {
+      throw redirect(302, "/login");
+    }
+
+    try {
+      const dbAccount = await db.query.account.findFirst(
+        { where: and(eq(account.id, params.id), eq(account.deleted, false)) },
+      );
+
+      if (!dbAccount) return;
+
+      await db.update(userConfig)
+        .set({ defaultAccountId: null })
+        .where(
+          and(
+            eq(userConfig.userId, session.user.userId),
+            eq(userConfig.defaultAccountId, dbAccount.id),
+          ),
+        );
+
+      await db.update(account)
+        .set({ deleted: true })
+        .where(eq(account.id, params.id));
+    } catch (e) {
+      throw error(500, "Failed to delete account");
+    }
+
+    // throw redirect(302, `/app/accounts/${params.id}`);
+  },
+  restore: async event => {
+    if (await accountUpdateLimiter.isLimited(event)) throw error(429, "Too many requests");
+
+    const { locals, params } = event;
+    const session = await locals.auth.validate();
+
+    if (!session || (EMAIL_VERIFICATION && !session.user.verified)) {
+      throw redirect(302, "/login");
+    }
+
+    try {
+      const dbAccount = await db.query.account.findFirst(
+        { where: and(eq(account.id, params.id), eq(account.deleted, true)) },
+      );
+
+      if (!dbAccount) return;
+
+      await db.update(account)
+        .set({ deleted: false })
+        .where(eq(account.id, params.id));
+    } catch (e) {
+      throw error(500, "Failed to delete account");
+    }
+
+    // throw redirect(302, `/app/accounts/${params.id}`);
+  },
+  permanentlyDelete: async event => {
+    if (await accountUpdateLimiter.isLimited(event)) throw error(429, "Too many requests");
+
+    const { locals, params } = event;
+    const session = await locals.auth.validate();
+
+    if (!session || (EMAIL_VERIFICATION && !session.user.verified)) {
+      throw redirect(302, "/login");
+    }
+
+    try {
+      const dbAccount = await db.query.account.findFirst(
+        { where: and(eq(account.id, params.id), eq(account.deleted, true)) },
+      );
+
+      if (!dbAccount) throw error(400, "Cannot permanently delete active account.");
+
+      await db.update(userConfig)
+        .set({ defaultAccountId: null })
+        .where(
+          and(
+            eq(userConfig.userId, session.user.userId),
+            eq(userConfig.defaultAccountId, dbAccount.id),
+          ),
+        );
+
+      await db.delete(account)
+        .where(eq(account.id, params.id));
+    } catch (e) {
+      console.error("Failed to permanently delete account");
+      console.error(e);
+
+      throw error(500, "Failed to delete account");
+    }
+
+    // throw redirect(302, `/app/accounts/${params.id}`);
+  },
+};
---
+++ src/routes/app/accounts/[id]/+page.svelte
@@ -0,0 +1,133 @@
+<script lang="ts">
+  import { superForm } from "sveltekit-superforms/client";
+  import type { PageData } from "./$types";
+  import FormError from "components/FormError.svelte";
+  import FormSuccess from "components/FormSuccess.svelte";
+  import { writable } from "svelte/store";
+
+  export let data: PageData;
+
+  const changed = writable(false);
+
+  const {
+    form,
+    enhance,
+    message,
+    delayed,
+    errors,
+    constraints,
+  } = superForm(data.form, {onUpdated: () => {$changed = false;}});
+</script>
+
+<svelte:head>
+  <meta name="robots" content="noindex" />
+  <title>Chatter | Account | {data.account.name}</title>
+</svelte:head>
+
+<div class="flex h-screen mx-auto my-auto max-w-sm items-center flex-col justify-center">
+  <form use:enhance method="post" action="?/save" class="w-full">
+    <label class="label mb-5">
+      <span>Account name</span>
+
+      <input
+        name="name"
+        class="input"
+        type="text"
+        placeholder="John Doe"
+        disabled={$delayed || data.account.deleted}
+        on:change={(_) => {$changed = true;}}
+        bind:value={$form.name}
+        {...$constraints.name}
+      />
+
+      {#if $errors.name}
+        <FormError error={$errors.name} />
+      {/if}
+    </label>
+
+    <label class="label mb-5">
+      <span>Secret API key</span>
+
+      <input
+        name="key"
+        class="input"
+        type="password"
+        placeholder="SECRET API KEY"
+        disabled={$delayed || data.account.deleted}
+        on:change={(_) => {$changed = true;}}
+        bind:value={$form.key}
+        {...$constraints.key}
+      />
+
+      {#if $errors.key}
+        <FormError error={$errors.key} />
+      {/if}
+    </label>
+
+    <label class="label mb-5">
+      <span>Model</span>
+
+      <select
+        class="select"
+        name="chatModelId"
+        disabled={$delayed || data.account.deleted}
+        on:change={(_) => {$changed = true;}}
+        bind:value={$form.chatModelId}
+      >
+        {#each data.chatModels as model}
+          <option value={model.id}>{model.displayName}</option>
+        {/each}
+      </select>
+
+      {#if $errors.chatModelId}
+        <FormError error={$errors.chatModelId} />
+      {/if}
+    </label>
+
+    {#if $errors._errors}
+      <FormError error={$errors._errors} />
+    {/if}
+
+    {#if $message}
+      <FormSuccess message={$message} />
+    {/if}
+
+    {#if !data.account.deleted}
+      <input
+        type="submit"
+        value={$delayed ? "Saving..." : "Save"}
+        class={`btn mt-5 w-full ${$delayed || !$changed ? "variant-filled-surface" : "variant-filled"}`}
+        disabled={$delayed || !$changed}
+      />
+    {/if}
+  </form>
+
+  {#if !data.account.deleted}
+    <form action="?/delete" method="post" class="w-full">
+      <input
+        type="submit"
+        value="Delete account"
+        class={`btn mt-5 w-full ${$delayed ? "variant-filled-surface" : "variant-filled-error"}`}
+        disabled={$delayed}
+      />
+    </form>
+  {:else}
+    <form action="?/restore" method="post" class="w-full">
+      <input
+        type="submit"
+        value="Restore account"
+        class={`btn mt-5 w-full ${$delayed ? "variant-filled-surface" : "variant-filled-primary"}`}
+        disabled={$delayed}
+      />
+    </form>
+
+    <form action="?/permanentlyDelete" method="post" class="w-full">
+      <input
+        type="submit"
+        value="Permanently delete account"
+        class={`btn mt-5 w-full ${$delayed ? "variant-filled-surface" : "variant-filled-error"}`}
+        disabled={$delayed}
+      />
+    </form>
+  {/if}
+</div>
