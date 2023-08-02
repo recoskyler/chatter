0f85f7abe46e6d35a538e39642cd308b87faa100
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-08-02 16:57:06 +0000

    Made homepage presentable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,32 @@
 <script lang="ts">
-  import { version } from '$app/environment';
+  import { version } from "$app/environment";
+  import Logo from "components/Logo.svelte";
 </script>
 
-<div class="flex my-auto mx-auto items-center justify-center text-center gap-5">
-  <a href="/app/profile" class="anchor">Profile</a>
-  <a href="/app" class="anchor">App</a>
-  <p>v{version}</p>
-</div>
+<div
+  class="flex flex-col my-auto mx-auto items-center justify-center text-center gap-5"
+>
+  <Logo />
 
+  <p>Free and open-source ChatGPT client</p>
+
+  <div class="flex flex-row gap-5 items-center justify-center">
+    <a href="/app" class="btn variant-filled">Launch Chatter</a>
+    <a href="/app" class="btn variant-ghost-tertiary">View source code</a>
+  </div>
+
+  <div class="flex flex-row gap-5 items-center justify-center text-slate-500 dark:text-slate-400">
+    <span>
+      <span>By </span>
+      <a
+        href="https://github.com/recoskyler"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="anchor"
+      >
+        recoskyler
+      </a>
+    </span>
+    <p> v{version}</p>
+  </div>
+</div>
