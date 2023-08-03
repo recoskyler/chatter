37090be022125ee3fb91438f9984e31416582bac
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2023-08-03 09:06:29 +0000

    Moved cookie functions to helpers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,33 +15,10 @@
   import hljs from "highlight.js";
   import { storeHighlightJs } from "@skeletonlabs/skeleton";
   import { onMount } from "svelte";
+  import { getCookie, setCookie } from "$lib/functions/helper";
 
   storeHighlightJs.set(hljs);
   storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
-
-  function setCookie(name: string, value: string, expirationInDays: number) {
-    const d = new Date();
-    d.setTime(d.getTime() + expirationInDays * 24 * 60 * 60 * 1000);
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-  }
-
-  function getCookie(name: string) {
-    let cookieName = name + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(";");
-
-    for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == " ") {
-        c = c.substring(1);
-      }
-      if (c.indexOf(cookieName) == 0) {
-        return c.substring(cookieName.length, c.length);
-      }
-    }
-    return "";
-  }
 
   onMount(() => {
     if (getCookie("disclaimer-dismissed") === "") {
